899f5189d93e5b6eb3b2a0f4faaee4e370533235
inomedia01 <david.aprilianto52@smk.belajar.id>
2025-07-21 21:46:41 -0700

    Add files via upload

---
+++ dataproduk.js
@@ -0,0 +1,214 @@
+// dataproduk.js
+window.productsData = [
+    {
+        id: "1",
+        name: "Router WIFI 6 AX3000",
+        price: 1299000,
+        oldPrice: 1499000,
+        category: "Router & Modem",
+        image: "https://images.unsplash.com/photo-1626743995788-8b701c5e5a0b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=80",
+        code: "RT-001",
+        description: "Router WIFI 6 terbaru dengan kecepatan hingga 3000Mbps, cocok untuk kebutuhan rumah dan kantor kecil.",
+        badge: "Terlaris"
+    },
+    {
+        id: "2",
+        name: "Tiang 2,5 x 6 meter (polos)",
+        price: 350000,
+        category: "Bengkel Las",
+        image: "bengkel las 1.jpg",
+        code: "",
+        description: "",
+        
+    },
+    {
+        id: "3",
+        name: "DEAD END",
+        price: 20000,
+        category: "Bengkel Las",
+        image: "bengkel las 2.jpg",
+        code: "",
+        description: "",
+        
+    },
+        {
+        id: "4",
+        name: "Tiang 3 + 2,5 X 2,3 mm (6 + 1 meter)",
+        price: 550000,
+        category: "Bengkel Las",
+        image: "bengkel las 3.jpg",
+        code: "",
+        description: "",
+        
+    },
+        {
+        id: "5",
+        name: "PAKAN LAS BRIDGE 3,2 X 350 MM",
+        price: 70000,
+        category: "Bengkel Las",
+        image: "bengkel las 4.jpg",
+        code: "",
+        description: "",
+        
+    },
+        {
+        id: "6",
+        name: "STAINLESS BELT POTONGAN",
+        price: 4000,
+        category: "Bengkel Las",
+        image: "none.png",
+        code: "",
+        description: "",
+        
+    },
+        {
+        id: "7",
+        name: "KUAS ETERNA 4",
+        price: 19500,
+        category: "Bengkel Las",
+        image: "bengkel las 6.jpg",
+        code: "",
+        description: "",
+        
+    },
+        {
+        id: "8",
+        name: "BEJI GOSOK KINIX 4 X 6 @ 200",
+        price: 8500,
+        category: "Bengkel Las",
+        image: "bengkel las 7.jpg",
+        code: "",
+        description: "",
+        
+    },
+        {
+        id: "9",
+        name: "PAKAN LAS RB 2,6 MM @ 20",
+        price: 178000,
+        category: "Bengkel Las",
+        image: "bengkel las 8.jpg",
+        code: "",
+        description: "",
+        
+    },
+        {
+        id: "10",
+        name: "BATU GERINDA ENKA POTONG 4 X 1,2",
+        price: 3500,
+        category: "Bengkel Las",
+        image: "bengkel las 9.jpg",
+        code: "",
+        description: "",
+        
+    },
+        {
+        id: "11",
+        name: "u case",
+        price: 600,
+        category: "Bengkel Las",
+        image: "bengkel las 10.jpg",
+        code: "",
+        description: "",
+        
+    },
+        {
+        id: "12",
+        name: "DEMPUL TANURI (KG) + H @12",
+        price: 50000,
+        category: "Bengkel Las",
+        image: "bengkel las 11.jpg",
+        code: "",
+        description: "",
+        
+    },
+        {
+        id: "13",
+        name: "EPOXY DANA GREY 534-0620+H",
+        price: 85000,
+        category: "Bengkel Las",
+        image: "bengkel las 12.jpg",
+        code: "",
+        description: "",
+        
+    },
+        {
+        id: "14",
+        name: "SLACK HANGER",
+        price: 50000,
+        category: "Bengkel Las",
+        image: "bengkel las 13.jpg",
+        code: "",
+        description: "",
+        
+    },
+        {
+        id: "15",
+        name: "CAT ZINC CR BEL MAS (GLN) GREEN",
+        price: 210000,
+        category: "Bengkel Las",
+        image: "bengkel las 14.jpg",
+        code: "",
+        description: "",
+        
+    },
+        {
+        id: "16",
+        name: "PAKAN LAS NIKO RD 460 2MM",
+        price: 75000,
+        category: "Bengkel Las",
+        image: "bengkel las 15.jpg",
+        code: "",
+        description: "",
+        
+    },
+        {
+        id: "17",
+        name: "Cat EMCO hitam 1 kg",
+        price: 75000,
+        category: "Bengkel Las",
+        image: "bengkel las 16.jpg",
+        code: "",
+        description: "",
+        
+    },
+        {
+        id: "18",
+        name: "Stainless belt kecil",
+        price: 120000,
+        category: "Bengkel Las",
+        image: "bengkel las 17.jpg",
+        code: "",
+        description: "",
+        
+    },
+        {
+        id: "19",
+        name: "Stainless belt besar",
+        price: 140000,
+        category: "Bengkel Las",
+        image: "bengkel las 18.jpg",
+        code: "",
+        description: "",
+        
+    },
+        {
+        id: "20",
+        name: "WD CUTTING 4 inch x 1, 2 x 1,6 mm",
+        price: 3500,
+        category: "Bengkel Las",
+        image: "bengkel las 19.jpg",
+        code: "",
+        description: "",
+        
+    },
+        {
+        id: "21",
+        name: "WD CUTTING besar 14 inch",
+        price: 38000,
+        category: "Bengkel Las",
+        image: "bengkel las 20.jpg",
+        code: "",
+        description: "",
+        
+    }
+];
