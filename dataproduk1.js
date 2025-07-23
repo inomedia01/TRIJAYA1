8fe68f56a4ef8843ef852d59a1445c2c5e824f51
inomedia01 <david.aprilianto52@smk.belajar.id>
2025-07-23 00:20:17 -0700

    Add files via upload

---
+++ dataproduk1.js
@@ -0,0 +1,53 @@
+window.productsData = [
+    {
+        id: "1",
+        name: "Router WIFI 6 AX3000",
+        price: 1299000,
+        oldPrice: 1499000,
+        category: "Router & Modem",
+        image: "https://images.unsplash.com/photo-1626743995788-8b701c5e5a0b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=80",
+        images: [
+            "https://images.unsplash.com/photo-1626743995788-8b701c5e5a0b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=80",
+            "none.png",
+            "none.png"
+        ],
+        code: "RT-001",
+        description: "Router WIFI 6 terbaru dengan kecepatan hingga 3000Mbps, cocok untuk kebutuhan rumah dan kantor kecil.",
+        badge: "Terlaris",
+        rating: 4.5,
+        reviews: 24
+    },
+    {
+        id: "2",
+        name: "Tiang 2,5 x 6 meter (polos)",
+        price: 350000,
+        category: "Bengkel Las",
+        image: "bengkel las 1.jpg",
+        images: [
+            "bengkel las 1.jpg",
+            "none.png",
+            "none.png"
+        ],
+        code: "",
+        description: "",
+        rating: 4.0,
+        reviews: 8
+    },
+    // Data produk lainnya dengan struktur yang sama...
+    {
+        id: "21",
+        name: "WD CUTTING besar 14 inch",
+        price: 38000,
+        category: "Bengkel Las",
+        image: "bengkel las 20.jpg",
+        images: [
+            "bengkel las 20.jpg",
+            "none.png",
+            "none.png"
+        ],
+        code: "",
+        description: "",
+        rating: 3.8,
+        reviews: 5
+    }
+];
